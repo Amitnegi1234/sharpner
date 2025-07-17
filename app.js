236f61b089ed7bdd005a70f2f73c5d250f0ba55a
Amitnegi1234 <132690578+Amitnegi1234@users.noreply.github.com>
2025-07-17 16:47:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 import express from "express";
 const app = express();
 
-// Middleware to parse JSON body
-app.use(express.json());
 
-// Products Routes
-app.get("/products", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Here is the list of all products.");
 });
 
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
+app.get("/user/:userId", (req, res) => {
+  const id=req.params.userId;
+  const name=req.query.name;
+  const age= req.query.age;
+  const role =req.query.role;
+  res.send(`id is ${id} , name is ${name}, age is ${age} , role is ${role}`);
 });
 
-// Categories Routes
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-});
 
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-});
 
-// Wildcard Route (404 Handler)
-app.use((req, res) => {
-  res.status(404).send("<h1>404 - Page Not Found</h1>");
-});
-
-// Start server on port 4000
-app.listen(4000, () => {
-  console.log("✅ Server running on http://localhost:4000");
+app.listen(3000, () => {
+  console.log("✅ Server running on http://localhost:3000");
 });
