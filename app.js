77d8ba22c3f598b1179db72ed90a1e6080771ca4
Amitnegi1234 <132690578+Amitnegi1234@users.noreply.github.com>
2025-07-17 11:48:21 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import express from "express";
-const app=express();
-app.use((req,res,next)=>{
-    res.send("hello");
-})
+const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server is up and running on port 3000! Ready to handle requests');
-})
+// Orders routes
+app.get("/orders", (req, res, next) => {
+  res.send("Here is the list of all orders");
+});
+
+app.post("/orders", (req, res, next) => {
+  res.send("A new order has been created.");
+});
+
+// Users routes
+app.get("/users", (req, res, next) => {
+  res.send("Here is the list of all users");
+});
+
+app.post("/users", (req, res, next) => {
+  res.send("A new user has been added");
+});
+
+app.listen(3000, () => {
+  console.log("Server is up and running on port 3000! Ready to handle requests");
+});
